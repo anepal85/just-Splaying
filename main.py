897e308fa39f1cbb55925953c6fd282acf3b6c1e
anepal85 <41636267+anepal85@users.noreply.github.com>
2022-08-06 21:39:57 +0200

    Create main.py
    Calls splay tree

---
+++ main.py
@@ -0,0 +1,13 @@
+t=SplayTree(10,10)
+for i in range(9,4,-1):
+  t.insert(i,i)
+    
+##t.insert(4,4)
+##t.insert(2,2)
+##t.insert(14,14)
+##t.insert(11,11)
+##t.insert(18,18)
+##t.insert(3,3)
+##t.insert(13,13)
+##t.insert(12,12)
+##t.insert(15,15)
